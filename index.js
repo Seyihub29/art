5c58d69c4c08b67f425b05d76cfe95c085186cdf
Abass Temilade <oluwaseyiabass123@gmail.com>
2025-09-11 09:47:42 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// const { name } = require('ejs')
+const express = require('express')
+const app = express()
+// require('ejs')
+// app.set('view engine', 'ejs')
+const port = 3200
+
+
+const nigerianArtiste = [
+    {name: 'Anendlessocean', songTitle: 'Juba', songUrl:'https://open.spotify.com/track/3y8fl4vQjkHjOaHc0nHmXU?si=57eb1bfb84284f86', yearofRelease:'2024'},
+    {name: 'Pelumideborah', songTitle: 'Gbogbonise', songUrl:'https://open.spotify.com/track/2g5SqiHdJjOPucWuHnzB4k?si=60e9613d19e74071', yearofRelease:'2025'},
+    {name: 'Limoblaze', songTitle: 'Calling(bless me)', songUrl:'https://open.spotify.com/track/70phX1Md7Hertk651Q06Z2?si=0fc759ff97da474e', yearofRelease:'2025'},
+    {name: 'Paultomisin', songTitle: 'Iyin ye', songUrl:'https://open.spotify.com/track/2OmxCzeLuekhaX7nSvzhax?si=4a64f417d8814463', yearofRelease:'2025'}
+]
+
+
+app.get('/users', (req,res) =>{
+    res.send(nigerianArtiste)
+})
+
+app.listen(port, () =>{
+    console.log(`server running on localhost:$(port)`);
+    
+})
